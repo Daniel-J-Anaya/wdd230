445567801b0f552186013503f350f3bae45dbb12
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2023-11-12 13:41:08 -0700

    Week05

---
+++ week05/scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+
+const url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}';
